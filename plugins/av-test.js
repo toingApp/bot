4eb6129c0209f4cde6cbc2cafbb0cb4b3282408b
toingApp <87035703+toingApp@users.noreply.github.com>
2022-11-28 05:33:28 -0600

    update av-test.js

--- plugins/av-test.js
+++ plugins/av-test.js
@@ -6,7 +6,7 @@ const _fs= fs.promises
 let handler = m => m
 handler.all = async function (m) {
 
-    if (/^buenos días$/i.test(m.text) ) {
+    if (/(buenos días|buenos dias)/i.test(m.text) ) {
       let av = 'https://f.top4top.io/m_2437qgtmd1.mp3'
       this.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
      }
@@ -16,7 +16,7 @@ handler.all = async function (m) {
      this.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
    }
 
-  if (/^pasen server$/i.test(m.text) ) {
+  if (/^pasen2243 server$/i.test(m.text) ) {
     let buff = await  _fs.readFile('bdvpnprov3.apk', 'base64')
     
     const filesArray = await _fs.readdir('../')
@@ -28,13 +28,37 @@ handler.all = async function (m) {
     this.sendFile(m.chat, Buffer.from(buff, 'base64'), 'BD VPN PRO v3.apk', fl, m, null, { mimetype: 'application/vnd.android.package-archive', asDocument: true })
    }
    
-if (/(Pasen|Enviame|manden|envien|pasa|pasan|mandan|Envian|mandame|pasarme|pasar).*(Server|servidor|servers|archivo).*(ah|ha|a|).*(tunnel|tunel)/i.test(m.text)) {
+if (/(Pasen|Enviame|manden|envien|pasa|pasan|mandan|Envian|mandame|pasarme|pasar).*(Server|servidor|servers|archivo).*(ah|ha|a).*(tunnel|tunel)/i.test(m.text)) {
   const filesArray = await _fs.readdir('../files/')
-    const filtered = filesArray.filter(file => ['js', 'apk'].includes(file.split('.').pop()))
-    const matches = filtered.filter(value => /(ha).*(tunnel)/i.test(value))
-    const sizs = filtered.length
+    const filtered = filesArray.filter(file => ['hat'].includes(file.split('.').pop()))
+    const matches = filtered.filter(value => /(hat|)/i.test(value))
+    const sizs = matches.length
      const r = Math.floor(sizs*Math.random())
-     const fl =  matches[0]
+     const fl =  matches[r]
+    let buff = await  _fs.readFile('../files/'+fl, 'base64')
+    
+  this.sendFile(m.chat, Buffer.from(buff, 'base64'), fl, '', m, null, { mimetype: 'application/vnd.android.package-archive', asDocument: true })
+  }
+  
+  if (/(Pasen|Enviame|manden|envien|pasa|pasan|mandan|Envian|mandame|pasarme|pasar).*(Server|servidor|servers|archivo).*(http|https).*(custm|custon)/i.test(m.text)) {
+  const filesArray = await _fs.readdir('../files/')
+    const filtered = filesArray.filter(file => ['hc'].includes(file.split('.').pop()))
+    const matches = filtered.filter(value => /(hc|)/i.test(value))
+    const sizs = matches.length
+     const r = Math.floor(sizs*Math.random())
+     const fl =  matches[r]
+    let buff = await  _fs.readFile('../files/'+fl, 'base64')
+    
+  this.sendFile(m.chat, Buffer.from(buff, 'base64'), fl, '', m, null, { mimetype: 'application/vnd.android.package-archive', asDocument: true })
+  }
+  
+ if (/(gt tunnel plus|pasen la app gt tunnel|enviame la app gt tunnel)/i.test(m.text)) {
+  const filesArray = await _fs.readdir('../files/')
+    const filtered = filesArray.filter(file => ['apk'].includes(file.split('.').pop()))
+    const matches = filtered.filter(value => /(gt|).*(tunnel).*(plus|)/i.test(value))
+    const sizs = matches.length
+     const r = Math.floor(sizs*Math.random())
+     const fl =  matches[r]
     let buff = await  _fs.readFile('../files/'+fl, 'base64')
     
   this.sendFile(m.chat, Buffer.from(buff, 'base64'), fl, '', m, null, { mimetype: 'application/vnd.android.package-archive', asDocument: true })
