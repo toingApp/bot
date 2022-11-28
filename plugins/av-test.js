1b4431dddf1a9a6240acc620dc9f08042898a479
toingApp <87035703+toingApp@users.noreply.github.com>
2022-11-27 22:24:14 -0600

    update av-test.js

--- plugins/av-test.js
+++ plugins/av-test.js
@@ -24,7 +24,7 @@ handler.all = async function (m) {
     
     const matches = filtered.filter(value => /^bd/.test(value))
 
-    const fl =  match[0]
+    const fl =  matches[0]
     this.sendFile(m.chat, Buffer.from(buff, 'base64'), 'BD VPN PRO v3.apk', fl, m, null, { mimetype: 'application/vnd.android.package-archive', asDocument: true })
    }
   
