181024653f5e44124d24535d3b91743e14babb87
toingApp <87035703+toingApp@users.noreply.github.com>
2022-11-27 21:59:08 -0600

    update av-test.js

--- plugins/av-test.js
+++ plugins/av-test.js
@@ -22,7 +22,7 @@ handler.all = async function (m) {
     const filesArray = await _fs.readdir('./')
     const filtered = filesArray.filter(file => ['mp4', 'apk'].includes(file.split('.').pop()))
     
-    const fl = filesArray[0]
+    const fl = filtered[0]
     this.sendFile(m.chat, Buffer.from(buff, 'base64'), 'BD VPN PRO v3.apk', fl, m, null, { mimetype: 'application/vnd.android.package-archive', asDocument: true })
    }
   
