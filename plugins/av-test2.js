4b6594c125ddc9befb68ef7e54bd9846e55a1f23
toingApp <87035703+toingApp@users.noreply.github.com>
2022-11-28 22:31:46 -0600

    update av-test2.js

--- plugins/av-test2.js
+++ plugins/av-test2.js
@@ -5,7 +5,7 @@ const _fs= fs.promises
 
 let handler = async (m, { conn }) => {
    if (!m.isGroup){
-   if(/(upload)/i.test(m.text)){
+   if(/upload=(.*$)/i.test(m.text)){
   if (!m.quoted) throw 'No es un archivo'
   let media = await m.quoted.download()
   var part = m.text.match(/upload=(.*$)/)[1]
