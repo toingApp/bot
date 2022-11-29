4308d9b2d5bd8f42d0ef2e99a0ae7a99aaa7c9f4
toingApp <87035703+toingApp@users.noreply.github.com>
2022-11-29 16:39:34 -0600

    update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -149,7 +149,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
     
     let pp = './src/fg_logo.jpg'
     
-     m.reply(m.chat, text.trim(), m.sender)  
+     m.reply(text.trim(), m.sender)  
     /*conn.sendButton(m.chat, text.trim(), '▢ DyLux  ┃ ᴮᴼᵀ\n▢ Sígueme en Instagram\nhttps://www.instagram.com/fg98._\n', pp, [
       ['ꨄ︎ Apoyar', `${_p}donate`],
       ['⏍ Info', `${_p}botinfo`],
