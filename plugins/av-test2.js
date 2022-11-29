7061f772a735489c20e801d46f7d972ebfe39238
toingApp <87035703+toingApp@users.noreply.github.com>
2022-11-29 17:12:50 -0600

    update av-test2.js

--- plugins/av-test2.js
+++ plugins/av-test2.js
@@ -6,19 +6,9 @@ const _fs= fs.promises
 let handler = m => m
 handler.all = async function (m) {
 	
-   if (!m.quoted) throw '✳️ Responde a un sticker animado'
-     let q = m.quoted ? m.quoted : m
-   /* let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
-    if (!/video|audio/.test(mime)) throw `✳️ Responda al video o nota de voz que desea convertir a mp3 con el comando :\n\n*${usedPrefix + command}*`*/
-   if(/(upload=)/i.test(m.text)){
+ if(/(mierda|puta)/i.test(m.text)){
    m.react(rwait)
-   let media = await q.download()
-    if (!media) throw '❎ Error al descargar medios'
-   const sd = m.text
-   const fi = sd.split('=')[1]
-   /*await _fs.writeFile('../files/'+fi, media)*/
-   
-     this.sendFile(m.chat, Buffer.from(media, 'base64'), fi, '', m, null, { mimetype: '*/*', asDocument: true })
+   m.reply(` %name% haz sido advertido 0 insultos!! la próxima  seras expulsado `)
   }
   return !0
 }
