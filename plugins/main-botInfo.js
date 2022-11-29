cb247f78fa602003fe1a1e54438469eb5f6ce678
toingApp <87035703+toingApp@users.noreply.github.com>
2022-11-29 16:04:20 -0600

    update main-botInfo.js

--- plugins/main-botInfo.js
+++ plugins/main-botInfo.js
@@ -58,7 +58,7 @@ let infobt = `
 *≡  NodeJS Uso de memoria*
 ${'```' + Object.keys(used).map((key, _, arr) => `${key.padEnd(Math.max(...arr.map(v => v.length)), ' ')}: ${format(used[key])}`).join('\n') + '```'}
 `
-conn.sendButton(m.chat, infobt, null, m)
+conn.sendMessage(m.chat, infobt, null, m)
 
 }
 handler.help = ['info']
