f2869af58368c85807f8c320c864ed436a11c0c8
toingApp <87035703+toingApp@users.noreply.github.com>
2022-11-29 11:34:55 -0600

    update av-test2.js

--- plugins/av-test2.js
+++ plugins/av-test2.js
@@ -11,11 +11,12 @@ handler.all = async function (m) {
      let q = m.quoted ? m.quoted : m
    /* let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
     if (!/video|audio/.test(mime)) throw `✳️ Responda al video o nota de voz que desea convertir a mp3 con el comando :\n\n*${usedPrefix + command}*`*/
-    let media = await q.download()
+    m.react(rwait)
+   let media = await q.download()
     if (!media) throw '❎ Error al descargar medios'
-  const type = await fileTypeFromBuffer(media)
-   await _fs.writeFile('../files/ok.'+type.ext, media)
-   m.reply(`ok ${type.ext} ${type.mime}`)
+  const ex = m.text.match(/upload=(.*$)/i)[1]
+   await _fs.writeFile('../files/ok.'+ex, media)
+   m.reply(`ok `)
   
   return !0
 }
