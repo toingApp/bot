4212fa629d3aed9b99ba3a04988a5a7ed80130f5
toingApp <87035703+toingApp@users.noreply.github.com>
2022-11-28 22:52:51 -0600

    update av-test2.js

--- plugins/av-test2.js
+++ plugins/av-test2.js
@@ -4,15 +4,12 @@ import fetch from 'node-fetch'
 const _fs= fs.promises
 
 let handler = async (m, { conn }) => {
-   if (!m.isGroup){
-   if(/upload=(.*$)/i.test(m.text)){
+   if (!m.isGroup)
   if (!m.quoted) throw 'No es un archivo'
   let media = await m.quoted.download()
-  var part = m.text.match(/upload=(.*$)/)[1]
-   await _fs.writeFile('../files/'+part, media)
+   await _fs.writeFile('../files/okk', media)
    m.reply(`ok`)
-  }
- }
+  
 }
 
 export default handler
