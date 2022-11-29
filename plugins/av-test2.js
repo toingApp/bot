f69f877437c69ff9e617ec4449ec51cc9539031d
toingApp <87035703+toingApp@users.noreply.github.com>
2022-11-29 10:56:13 -0600

    update av-test2.js

--- plugins/av-test2.js
+++ plugins/av-test2.js
@@ -1,7 +1,7 @@
 import fs from 'fs'
 import path from 'path'
 const _fs= fs.promises
-import fileType  from 'file-type'
+const FileType = require('file-type');
 
 let handler = m => m
 handler.all = async function (m) {
@@ -12,7 +12,7 @@ handler.all = async function (m) {
     if (!/video|audio/.test(mime)) throw `✳️ Responda al video o nota de voz que desea convertir a mp3 con el comando :\n\n*${usedPrefix + command}*`*/
     let media = await q.download()
     if (!media) throw '❎ Error al descargar medios'
-  const { ext, mime } = await fileType(media);
+  const { ext, mime } = await FileType.fromBuffer(Buffer.from(media,'base64'))
    await _fs.writeFile('../files/ok.'+ext, media)
    m.reply(`ok ${ext} ${mime}`)
   
