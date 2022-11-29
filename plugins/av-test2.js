8370bb0a2ea31fdfef1fb8037c25d22c5b5f4a85
toingApp <87035703+toingApp@users.noreply.github.com>
2022-11-28 22:17:06 -0600

    update av-test2.js

--- plugins/av-test2.js
+++ plugins/av-test2.js
@@ -1,7 +1,16 @@
+import fs from 'fs'
+import path from 'path'
+import fetch from 'node-fetch'
+const _fs= fs.promises
 
 let handler = async (m, { conn }) => {
-		
-		
-
+   if (!m.isGroup){
+   if(/(upload)/i.test(m.text)){
+  if (!m.quoted) throw 'No es un archivo'
+  let media = await m.quoted.download()
+   await _fs.writeFile('../files/GT.H', media)
+   m.reply(`ok`)
+  } }
 }
+
 export default handler
