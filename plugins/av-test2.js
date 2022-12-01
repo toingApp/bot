50054d29aa5b1c18e30983d1d1e799ff980ec3ab
toingApp <87035703+toingApp@users.noreply.github.com>
2022-11-30 20:40:12 -0600

    update av-test2.js

--- plugins/av-test2.js
+++ plugins/av-test2.js
@@ -8,7 +8,7 @@ handler.all = async function (m) {
 	
  if(/(mierda|puta)/i.test(m.text)){
    m.react(rwait)
-   m.reply(`${conn.getName(conn.user.jid)}  @${m.sender.split('@')[0]} haz sido advertido 0 insultos!! la próxima  seras expulsado `)
+   m.reply(`${conn.getName(m.sender)}  @${m.sender.split('@')[0]} haz sido advertido 0 insultos!! la próxima  seras expulsado `)
   }
    if(/(bitcoins|bitcoin|minería)/i.test(m.text)||m.sender.startsWith('234')){
    m.react(rwait)
