01693aa2749f3a808bbc4abdfe6a28319d139178
toingApp <87035703+toingApp@users.noreply.github.com>
2022-12-04 09:52:33 -0600

    update av-test2.js

--- plugins/av-test2.js
+++ plugins/av-test2.js
@@ -33,10 +33,14 @@ var url = "https://api.openai.com/v1/completions";
      }};
 
   var data = `{
-    "prompt": "que tal",
-    "temperature": 2.0,
-    "max_tokens": 200,
-    "model": "text-davinci-003"
+      "model": "text-davinci-003"
+    "prompt": "The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hola, como estas?\nAI:",
+    "temperature": 0.9,
+   "max_tokens": 150, 
+   "top_p": 1, 
+  "frequency_penalty": 0.0,
+ "presence_penalty": 0.6,
+ "stop": [" Human:", " AI:"]
   }`;
 
   xhr.send(data);
